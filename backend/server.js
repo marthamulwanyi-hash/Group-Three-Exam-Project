2d39d336cd8486c61d7774deaa680eb67d9f97ad
clancyjoseph456-blip <clancyjoseph456@gmail.com>
2026-04-09 20:22:27 +0100

    clancy Joseph

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
