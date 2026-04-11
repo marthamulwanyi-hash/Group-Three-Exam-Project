dbaa944656fe439864d33ee1ea47deb5ac9ea9c4
clancyjoseph456-blip <clancyjoseph456@gmail.com>
2026-04-11 20:04:49 +0100

    clancy joseph

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,12 @@
+// server.js
 const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const helmet = require('helmet');
-const morgan = require('morgan');
-
-dotenv.config();
-
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json());
-app.use(helmet());
-app.use(morgan('dev'));
+app.use(package.json());
 
-// Routes
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/groups', require('./routes/groupRoutes'));
-app.use('/api/sessions', require('./routes/sessionRoutes'));
-app.use('/api/posts', require('./routes/postRoutes'));
-
-// Test route
 app.get('/', (req, res) => {
-  res.send('API is running...');
+  res.send('Server is running...');
 });
 
-// 404 handler
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: 'Server error' });
-});
-
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
